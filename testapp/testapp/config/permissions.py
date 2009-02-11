69bf75af7cbef3c4b998bcafb98c223c5453fd4a
Alexandre Saunier <alexandre.saunier@camptocamp.com>
2009-02-11 17:10:03 +0000

    removed deprecated testapp
    git-svn-id: http://svn.mapfish.org/svn/mapfish/trunk/MapFish/server/python@1703 25cca10d-0837-0410-9e4f-92b931fda1bc

--- testapp/testapp/config/permissions.py
+++
@@ -1,8 +0,0 @@
-from authkit.permissions import HasAuthKitRole, ValidAuthKitUser
-from mapfish.controllers.security import Deny
-
-permissions = {
-    "application": ValidAuthKitUser(),
-    "application.widgets.edit": HasAuthKitRole("editor"),
-    "widgets.print.dpi.100": Deny()
-}
--- testapp/testapp/controllers/security.py
+++
@@ -1,9 +0,0 @@
-import logging
-
-from testapp.lib.base import *
-import mapfish.controllers.security
-
-log = logging.getLogger(__name__)
-
-class SecurityController(mapfish.controllers.security.SecurityController):
-    pass
