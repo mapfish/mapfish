212c544fc1e36b1f99ff3bed743d47594b99f275
fvanderbiest <francois.vanderbiest@camptocamp.com>
2010-11-17 12:19:05 +0000

    GPL to BSD migration - closes #608
    git-svn-id: http://svn.mapfish.org/svn/mapfish/framework/server/trunk@3659 25cca10d-0837-0410-9e4f-92b931fda1bc

--- setup.py
+++ setup.py
@@ -1,20 +1,29 @@
 # 
-# Copyright (C) 2009  Camptocamp
-#  
-# This file is part of MapFish Server
-#  
-# MapFish Server is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Lesser General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#  
-# MapFish Server is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Lesser General Public License for more details.
-#  
-# You should have received a copy of the GNU Lesser General Public License
-# along with MapFish Server.  If not, see <http://www.gnu.org/licenses/>.
+# Copyright (c) 2008-2011 Camptocamp.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+# 3. Neither the name of Camptocamp nor the names of its contributors may 
+#    be used to endorse or promote products derived from this software 
+#    without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+# DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #
 
 try:
@@ -42,7 +51,7 @@ if sys.version_info < (2, 5):
 
 setup(name                 = 'mapfish',
       version              = '2.0dev',
-      license              = 'LGPLv3',
+      license              = 'Modified BSD',
       install_requires     = requirements,
       zip_safe             = False,
       include_package_data = True,
@@ -55,7 +64,7 @@ setup(name                 = 'mapfish',
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)',
+        'License :: OSI Approved :: Modified BSD License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Framework :: Pylons',
